ccfc2f7bf817afdaa209f6fe3c35d71e8f3dae38
GusJS <16326176760@edu.udesc.br>
2022-10-02 19:57:09 -0300

    Criação de algumas Views e códigos.

--- projetoProg2/src/main/java/br/udesc/company/exercicios/projetoprog2/App.java
+++ projetoProg2/src/main/java/br/udesc/company/exercicios/projetoprog2/App.java
@@ -4,10 +4,18 @@
  */
 package br.udesc.company.exercicios.projetoprog2;
 
+
+
 /**
  *
  * @author sonho
  */
 public class App {
-    
+    public static void main(String[] args) {
+//        CadastroProdutoView cadastro = new CadastroProdutoView();
+//        cadastro.setVisible(true);
+        MainView main = new MainView();
+        main.setVisible(true);
+        
+    }
 }
