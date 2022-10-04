f36cc5d3581d08e5618d273bb983fede78097f19
LuizFJP <luizfjportela@gmail.com>
2022-10-03 21:00:53 -0300

    feat: login and subscribe views

---
+++ hermozotovisk/src/main/java/model/Cliente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author luizportel4
+ */
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
